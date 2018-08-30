78a4bb88085d2e375f3c0d9d28f7639213bb8677
jessye95 <jessye95@gmail.com>
2018-08-30 02:24:08 +0100

    keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '850388698547-9tc0ogehcr04ro8fn0cl72ta68a80f09.apps.googleusercontent.com',
+  googleClientSecret: 'AyJDAd-JxKEbd2f8d5v0eZpN',
+  mongoURI: 'mongodb://easymail:easymail1@ds125392.mlab.com:25392/easymail',
+  cookieKey: 'woohoo'
+}
