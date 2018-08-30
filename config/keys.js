361a0aa6b9de8a047eae857f24e49ca92eb72597
jessye95 <jessye95@gmail.com>
2018-08-30 02:52:35 +0100

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-  googleClientID: '850388698547-9tc0ogehcr04ro8fn0cl72ta68a80f09.apps.googleusercontent.com',
-  googleClientSecret: 'AyJDAd-JxKEbd2f8d5v0eZpN',
-  mongoURI: 'mongodb://easymail:easymail1@ds125392.mlab.com:25392/easymail',
-  cookieKey: 'woohoo'
+
+if (process.env.NODE_ENV ==== 'production') {
+  module.exports = require('./prod')
+}
+else {
+  module.exports = require('./dev')
 }
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+  module.exports = { 
+  googleClientID: '759498202674-h7242662rlvlek4p4a90u5jiif2ervnc.apps.googleusercontent.com' ,
+  googleClientSecret: 'tO7FSS6r4wqwQRitYb6DXItk',
+  mongoURI: 'mongodb://easymail-prod:easymail1@ds237072.mlab.com:37072/easymail-prod',
+  cookieKey: ''
+} 
